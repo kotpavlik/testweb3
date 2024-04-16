d823be658a4b0684069f8885d28e14e9ea9fd925
kotpavlik <kotpavlik88@gmail.com>
2024-04-16 20:46:49 +0300

    manifest

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,7 +5,7 @@ import { TonConnectButton, TonConnectUIProvider } from "@tonconnect/ui-react";
 const UserProfile = () => {
     return (
 
-        <TonConnectUIProvider manifestUrl="https://myapp.com/tonconnect-manifest.json" actionsConfiguration={{ twaReturnUrl: 'https://calc-jam-app.vercel.app/' }}>
+        <TonConnectUIProvider manifestUrl="https://testweb3-omega.vercel.app/tonconnect-manifest.json" actionsConfiguration={{ twaReturnUrl: 'https://calc-jam-app.vercel.app/' }}>
             <div>
                 Hello
                 <TonConnectButton className="m-2 " />
