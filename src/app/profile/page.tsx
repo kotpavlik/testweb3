5ba8c22c209194590e65db18f2ae76e3a08d916d
kotpavlik <kotpavlik88@gmail.com>
2024-04-16 22:13:15 +0300

    manifest1

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,7 +6,7 @@ const UserProfile = () => {
     return (
 
         <TonConnectUIProvider
-            manifestUrl="https://vercel.com/kotpavliks-projects/testweb3/9z1z3LrpHv7Vq26HsTCtGwNNCexe/source?f=src%2Ftonconnect-manifest.json"
+            manifestUrl="https://vercel.com/kotpavliks-projects/testweb3/Duwkpfbr57yf3TQbiKyJhMBmSXqV/source?f=src%2Ftonconnect-manifest.json"
             actionsConfiguration={{ twaReturnUrl: 'https://calc-jam-app.vercel.app/' }}>
             <div>
                 Hello
