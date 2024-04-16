6ae6a43e610d6e9f27e8029cbe8d40ef0e259ed2
kotpavlik <kotpavlik88@gmail.com>
2024-04-17 01:17:32 +0300

    hello_daun

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,7 +6,7 @@ const UserProfile = () => {
     return (
 
         <TonConnectUIProvider
-            manifestUrl="">
+            manifestUrl="https://vercel.com/kotpavliks-projects/testweb3/AF4TifKj6yT1z4KckscuxyVcupcz/source?f=src%2Ftonconnect-manifest.json">
             <div>
                 Hello
                 <TonConnectButton className="m-2 " />
