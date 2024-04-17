6a0034d5b028e446939d77e83ea91309667f1f5c
kotpavlik <kotpavlik88@gmail.com>
2024-04-17 19:52:22 +0300

    test_mini_app

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,13 +6,13 @@ const UserProfile = () => {
     return (
 
         <TonConnectUIProvider
-            manifestUrl="https://kotpavlik.github.io/testweb3/public/tonconnect-manifest.json">
+            manifestUrl="https://kotpavlik.github.io/testweb3/public/tonconnect-manifest.json"
+            actionsConfiguration={{ twaReturnUrl: 'https://calc-jam-app.vercel.app/' }}>
             <div>
                 Hello
                 <TonConnectButton className="m-2 " />
             </div>
         </TonConnectUIProvider >
-
 
     );
 }
