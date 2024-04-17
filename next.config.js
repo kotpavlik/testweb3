4d573db67c8c152860767aa80992058eb24de7b3
kotpavlik <kotpavlik88@gmail.com>
2024-04-17 19:19:47 +0300

    workflows

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    reactStrictMode: true
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    assetPrefix: './',
 }
 
 module.exports = nextConfig
